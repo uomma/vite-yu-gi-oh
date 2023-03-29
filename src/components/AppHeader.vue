6f9333accb41209900094175cf43fc0e26f61af9
uomma <marcelloleone975@gmail.com>
2023-03-30 01:18:41 +0200

    bootstrap

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,6 +8,17 @@ export default {
     <div>
         <header>
             <h1>Yu-Gi-Oh Api</h1>
+            <div class="dropdown my-5">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
         </header>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import './assets/scss/main.scss';
+
+
 import "@fontsource/montserrat"; // Defaults to weight 400.
-import App from './App.vue'
+import App from './App.vue';
+import * as bootstrap from 'bootstrap';
 
 createApp(App).mount('#app')
